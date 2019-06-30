723c58a7566c7184ff2b26d80cf6f454b31880e1
sticklercarla <carlastickler@gmail.com>
2019-06-30 15:00:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+
+const num1 = 31;
+const num2 = 2; 
+let multiply = num1 * num2;
+
+let random = Math.random();
+
+let mod = 29 % 5;
+
+let max = Math.max(1, 5, 10, 15, 20);
